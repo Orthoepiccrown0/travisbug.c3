a3d6dda52487ff65e321427dee4fa6bf61dedf5e
dimas <dmitro98.98@gmail.com>
2020-12-27 17:06:49 +0100

    Message

---
+++ src/main/java/it/unicam/travisbug/c3/controllers/Home.java
@@ -0,0 +1,18 @@
+package it.unicam.travisbug.c3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+    @GetMapping("/")
+    public String showHome(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String showLogin(){
+        return "login";
+    }
+}
---
+++ src/test/java/it/unicam/travisbug/c3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.travisbug.c3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
