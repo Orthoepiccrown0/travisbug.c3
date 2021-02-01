3c6590de6a119fe7b7132aa5df92e618f4ddb41b
dimas <dmitro98.98@gmail.com>
2021-02-01 18:14:49 +0100

    Profile page for every account type

--- src/main/java/it/unicam/travisbug/c3/controller/personal/area/Profile.java
+++ src/main/java/it/unicam/travisbug/c3/controller/personal/area/Profile.java
@@ -1,6 +1,10 @@
 package it.unicam.travisbug.c3.controller.personal.area;
 
+import it.unicam.travisbug.c3.model.*;
+import it.unicam.travisbug.c3.utils.AppCookies;
+import it.unicam.travisbug.c3.utils.DBManager;
 import it.unicam.travisbug.c3.utils.Roles;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -8,22 +12,55 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Profile {
+
+    DBManager dbManager;
+
+    @Autowired
+    public void setDbManager(DBManager dbManager) {
+        this.dbManager = dbManager;
+    }
+
     @GetMapping("/account")
     public String showAccount(Model model,
                               @CookieValue(value = "user_id", defaultValue = "") String userid,
-                              @CookieValue(value = "role", defaultValue = "") Roles role) {
-        if (role != null) {
-            if (role == Roles.CLIENT) {
-
-            } else if (role == Roles.COURIER) {
-
-            } else if (role == Roles.MERCHANT) {
-
-            } else if (role == Roles.EMPLOYEE) {
-
-            }
-
+                              @CookieValue(value = "role", defaultValue = "") String roleCookie) {
+        AppCookies appCookies = new AppCookies();
+        appCookies.checkLogged(model, userid, roleCookie);
+        if (roleCookie != null) {
+            Roles role = Roles.valueOf(roleCookie);
+            model.addAttribute("role", role);
+            setUser(model, userid, role);
         }
-        return "";
+        return "accounts/account";
+    }
+
+    private void setUser(Model model, String userid, Roles role) {
+        Employee employee;
+        Courier courier;
+        Client client;
+        Merchant merchant;
+        RegisteredUser user;
+        if (role == Roles.CLIENT) {
+            client = dbManager.getClientService().findById(userid).orElseThrow();
+            user = client;
+            model.addAttribute("client", client);
+            model.addAttribute("generic", user);
+        } else if (role == Roles.COURIER) {
+            courier = dbManager.getCourierService().findById(userid).orElseThrow();
+            user = courier;
+            model.addAttribute("courier", courier);
+            model.addAttribute("generic", user);
+        } else if (role == Roles.MERCHANT) {
+            merchant = dbManager.getMerchantService().findById(userid).orElseThrow();
+            user = merchant;
+            model.addAttribute("merchant", merchant);
+            model.addAttribute("generic", user);
+        } else if (role == Roles.EMPLOYEE) {
+            employee = dbManager.getEmployeeService().findById(userid).orElseThrow();
+            user = employee;
+            model.addAttribute("employee", employee);
+            model.addAttribute("generic", user);
+        }
+
     }
 }
