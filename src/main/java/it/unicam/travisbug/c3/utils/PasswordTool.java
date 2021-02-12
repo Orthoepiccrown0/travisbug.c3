afcc75984feb21e173b71c83b6f037bffdcc3eda
dimas <dmitro98.98@gmail.com>
2021-02-12 22:07:31 +0100

    PasswordTool update: added secret

--- src/main/java/it/unicam/travisbug/c3/utils/PasswordTool.java
+++ src/main/java/it/unicam/travisbug/c3/utils/PasswordTool.java
@@ -6,12 +6,15 @@ import java.security.NoSuchAlgorithmException;
 
 public class PasswordTool {
 
+    private static final String secret = "TR(r3nTS@QS,C}K.,FvE^jcjJ95{!Wbe>BVs{K";
+
     public static String getMD5String(String plainText){
         MessageDigest md = null;
         String myHash = null;
         try {
+            String secretText = plainText + secret;
             md = MessageDigest.getInstance("MD5");
-            md.update(plainText.getBytes());
+            md.update(secretText.getBytes());
             byte[] digest = md.digest();
             myHash = DatatypeConverter
                     .printHexBinary(digest).toUpperCase();
